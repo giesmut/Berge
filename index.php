c95d0e8194dde7fc696dd9ebe3cc045789798bd4
Chris Giesmut <cgiesinger69@gmail.com>
2017-02-10 11:59:25 +0100

    Blog-Informationen anzeigen: Verschiedene Infos in index.php hinzugefügt
    bloginfo ('name') > index.php
    <?php bloginfo('description') ?> > footer.php
    <?php bloginfo('url') ?> index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ get_header();
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="#">Project name</a>
+              <a class="navbar-brand" href="#"><?php bloginfo('name') ?></a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
